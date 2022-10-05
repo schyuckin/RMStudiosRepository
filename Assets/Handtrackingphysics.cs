48dd51debe7ac67ad8ceed7dd9c7241b9b264ace
Mandy Idema <m.e.idema@st.hanze.nl>
2022-10-05 09:16:48 +0200

    Optimization 2

--- Assets/Handtrackingphysics.cs
+++ Assets/Handtrackingphysics.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Handtrackingphysics : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Collider[] handCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        handCollider = GetComponentsInChildren<Collider>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        foreach (var item in handCollider)
+        {
+            
+            item.enabled = true;
+        }
+        foreach (var item in handCollider)
+        {
+            item.enabled = false;
+        }
+    }
+
+    public void EnableHandCollider()
+    {
+        foreach (var item in handCollider)
+        {
+            item.enabled = true;
+        }
+    }
+    public void DisableHandCollider()
+    {
+        foreach (var item in handCollider)
+        {
+            item.enabled = false;
+        }
+    }
+
+    public void EnabelHandColliderDelay(float delay)
+    {
+        Invoke("EnableHandCollider", delay);
     }
 }
