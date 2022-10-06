852e5119231a9e74e8adf4b8ae916ce27d13e2e4
Mandy Idema <m.e.idema@st.hanze.nl>
2022-10-06 16:03:47 +0200

    Added potion descriptions

--- Assets/Personal assets/Mandy/description_list.cs
+++ Assets/Personal assets/Mandy/description_list.cs
@@ -1,13 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class description_list : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string[] customerRequests;
+    public int potionPotency;
+
+    public int descriptionChosen;
+
+    private string requestChosen;
+
+    public GameObject descriptionShown;
+
+
     void Start()
     {
-        
+        descriptionChosen = Random.Range(0, 5);
+        requestChosen = customerRequests[descriptionChosen];
+        descriptionShown.GetComponent<TextMeshPro>().text = requestChosen;
+        switch (descriptionChosen)
+        {
+            case 0:
+                potionPotency = 1;
+                break;
+            case 1:
+                potionPotency = 2;
+                break;
+            case 2:
+                potionPotency = 3;
+                break;
+            case 3:
+                potionPotency = 2;
+                break;
+            case 4:
+                potionPotency = 3;
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +45,7 @@ public class description_list : MonoBehaviour
     {
         
     }
+
+ // Potion potency depends on the recipe
+ //The higher the potency the stronger are the effects
 }
