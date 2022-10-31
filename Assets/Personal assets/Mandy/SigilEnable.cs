46373c32332a0197302e54509956c128641fbd80
Kris <k.t.kurten@st.hanze.nl>
2022-10-31 15:51:02 +0100

    Script updates
    - Figuring out particles for touching sigils

--- Assets/Personal assets/Mandy/SigilEnable.cs
+++ Assets/Personal assets/Mandy/SigilEnable.cs
@@ -6,21 +6,21 @@ public class SigilEnable : MonoBehaviour
 {
 
     [SerializeField] private Collider[] differentSigils = new Collider[3];
-    [SerializeField] private GameObject[] differentParticles = new GameObject[3];
+    public ParticleSystem[] differentParticles = new ParticleSystem[3];
 
     [SerializeField] private GameObject flask;
-    private int sigTouched = 0; // Used in conjuction to prevent some potentially weird value passing stuff
+    [SerializeField] private int sigTouched = 0; // Used in conjuction to prevent some potentially weird value passing stuff
     public int sigilTouched = 0; // Used in conjuction to prevent some potentially weird value passing stuff
 
     public void Start()
     {
-        differentParticles[0].GetComponent<ParticleSystem>().Pause();
-        differentParticles[1].GetComponent<ParticleSystem>().Pause();
-        differentParticles[2].GetComponent<ParticleSystem>().Pause();
+        differentParticles[0].Pause();
+        differentParticles[1].Pause();
+        differentParticles[2].Pause();
+        Debug.Log("They paused");
     }
 
-
-    private void OnTriggerEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         //Saving the component for future use 
         ParticleSystem particles = GetComponentInChildren<ParticleSystem>();
@@ -29,35 +29,43 @@ public class SigilEnable : MonoBehaviour
         //or else we would get an error if we work with not existing component
         if (particles != null)
         {
-            if (collision.gameObject.tag == "potionSigil")
+            if (other.tag == "potionSigil")
             {
+
+                Debug.Log("It registeres touching the enchantment");
                 //particles.Play(); //Here we use the Play function to start the particle system
 
                 // [NOTE] I am not sure if the funky syntax works like that
-                if (collision.collider == differentSigils[0])
+                if (other == differentSigils[0])
                 {
+                    differentParticles[0].Play();
                     sigTouched = 1;
                 }
-                if (collision.collider == differentSigils[1])
+                if (other == differentSigils[1])
                 {
+                    differentParticles[1].Play();
                     sigTouched = 2;
                 }
-                if (collision.collider == differentSigils[2])
+                if (other == differentSigils[2])
                 {
+                    differentParticles[2].Play();
                     sigTouched = 3;
                 }
-                differentParticles[sigTouched - 1].GetComponent<ParticleSystem>().Play();
+
                 sigilTouched = sigTouched;
-            }
-            // [NOTE] Not sure about that either
-            if (collision.collider == flask.GetComponent<BoxCollider>())
-            {
-                // Passing stuff to flask
-                var passingToFlask = flask.GetComponent<flaskState>();
-                passingToFlask.sigilType = sigilTouched;
             }
             sigTouched = 0;
             particles.Stop(); //Here we use the Stop function to stop the particle system from playing
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "flask")
+        {
+            // Passing stuff to flask
+            var passingToFlask = flask.GetComponent<flaskState>();
+            passingToFlask.sigilType = sigilTouched;
             StartCoroutine(SigilDecay());
         }
     }
