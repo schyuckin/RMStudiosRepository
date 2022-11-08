85540c332034b85d99a2d05a914db7e47c4bc865
Kris <k.t.kurten@st.hanze.nl>
2022-11-08 16:05:17 +0100

    bUG FIXING
    im not bothering we did stuff

--- Assets/Personal assets/Aaron/Scripts/Teleport_Object.cs
+++ Assets/Personal assets/Aaron/Scripts/Teleport_Object.cs
@@ -24,20 +24,27 @@ public class Teleport_Object : MonoBehaviour
         {
             case "Red stuff":
                 other.transform.position = objectOrigins[0].transform.position;
-
+                Rigidbody rb1 = other.GetComponent<Rigidbody>();
+                rb1.velocity = new Vector3(0, 0, 0);
                 break;
             case "Yellow stuff":
                 other.transform.position = objectOrigins[1].transform.position;
+                Rigidbody rb2 = other.GetComponent<Rigidbody>();
+                rb2.velocity = new Vector3(0, 0, 0);
                 break;
             case "Blue stuff":
                 other.transform.position = objectOrigins[2].transform.position;
+                Rigidbody rb3 = other.GetComponent<Rigidbody>();
+                rb3.velocity = new Vector3(0, 0, 0);
                 break;
             case "Potion Flask":
                 other.transform.position = objectOrigins[3].transform.position;
+                Rigidbody rb4 = other.GetComponent<Rigidbody>();
+                rb4.velocity = new Vector3(0, 0, 0);
                 break;
         }
-        Rigidbody rb1 = other.GetComponent<Rigidbody>();
-        rb1.velocity = new Vector3(0, 0, 0);
+        
+        
     }
 
 
