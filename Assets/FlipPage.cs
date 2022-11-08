2142a1d664673d00a3d19af4b6d83913209cd1e6
Kris <k.t.kurten@st.hanze.nl>
2022-11-08 16:27:13 +0100

    Bug fixes
    - Delay on pressing the recipe button

--- Assets/FlipPage.cs
+++ Assets/FlipPage.cs
@@ -6,11 +6,35 @@ using UnityEngine;
 public class FlipPage : MonoBehaviour
 {
     public bool flipToRight = true;
+    public bool delay = false;
 
     public Book book;
 
     private void OnTriggerEnter(Collider other)
     {
-        book.FlipPage(flipToRight);
+        if (!delay)
+        {
+            delay = true;
+            book.FlipPage(flipToRight);
+            StartCoroutine(Wait());
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        delay = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        delay = false;
+
     }
 }
